e4cdd0de17bca2e41923e9bf428d689811b1c9a0
whysamir <csit23081025_samir@achsnepal.edu.np>
2025-07-31 16:27:33 +0545

    frontend starts added navbar and announcement

---
+++ src/app/components/Announcement.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+interface AnnouncementProps {
+  setShow: (show: boolean) => void;
+}
+
+export function Announcement({ setShow }: AnnouncementProps) {
+  return (
+    <motion.div
+      layout
+      initial={{
+        opacity: 1,
+        height: "auto",
+        paddingTop: 16,
+        paddingBottom: 16,
+      }}
+      animate={{
+        opacity: 1,
+        height: "auto",
+        paddingTop: 16,
+        paddingBottom: 16,
+      }}
+      exit={{
+        opacity: 0,
+        height: 0, // collapse height
+        paddingTop: 0, // remove vertical padding
+        paddingBottom: 0,
+      }}
+      transition={{ duration: 0.5, ease: "easeInOut" }}
+      style={{ transformOrigin: "top" }}
+      className="w-full overflow-hidden bg-orange-950 text-white"
+    >
+      <div className="max-w-7xl mx-auto flex justify-between items-center px-5 md:px-6">
+        {/* contact */}
+        <div className="flex items-center gap-2">
+          <div className="text-lg font-medium text-white">Support</div>
+          <div>(977)</div>
+          <p>9768445916</p>
+        </div>
+
+        {/* Main Text */}
+        <ul className="hidden md:flex">
+          <li className="cursor-pointer">
+            Sign up and <span className="font-semibold"> GET 25% OFF </span>
+            for your first order.
+            <span className="underline ml-1 text-[#f8fc80] underline-offset-4">
+              Sign up now
+            </span>
+          </li>
+        </ul>
+
+        <div className="cursor-pointer">
+          <span className="text-lg font-bold" onClick={() => setShow(false)}>
+            ✕
+          </span>
+        </div>
+      </div>
+    </motion.div>
+  );
+}
---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,72 @@
+"use client";
+import Image from "next/image";
+import { globalLayoutCss } from "../globalcss";
+import Menu from "./Menu";
+import { useState } from "react";
+import { motion } from "framer-motion";
+
+export default function Navbar() {
+  const [isRotated, setIsRotated] = useState(false);
+  const [show, setShow] = useState(false);
+
+  return (
+    <motion.nav
+      layout
+      //   initial={{ opacity: 1, y: 0 }}
+      //   animate={{ y: announcementClosed ? -10 : 0 }}
+      transition={{ duration: 0.6, ease: "easeInOut" }}
+      className="w-full  bg-white"
+    >
+      <div className={`${globalLayoutCss}`}>
+        {/* Logo */}
+        <div className="flex items-center gap-2">
+          <div className="w-6 h-6 md:w-8 md:h-8 bg-orange-950 rounded-full flex items-center justify-center text-white font-bold text-lg">
+            C
+          </div>
+          <span className="md:text-xl font-semibold">
+            Clothing<span className="text-[#4b2e1a]">.</span>
+          </span>
+        </div>
+
+        {/* Menu Links */}
+        <ul className="hidden md:flex space-x-8 text-gray-800 font-medium">
+          <li className="cursor-pointer hover:text-[#4b2e1a]">Home</li>
+          <li className="cursor-pointer hover:text-[#4b2e1a]">Shop</li>
+          <li className="cursor-pointer hover:text-[#4b2e1a]">Women</li>
+          <li className="cursor-pointer hover:text-[#4b2e1a]">Men</li>
+          <li className="cursor-pointer hover:text-[#4b2e1a]">About Us</li>
+        </ul>
+
+        {/* Icons */}
+        <div className="flex items-center gap-x-3 md:gap-x-6 text-gray-700 text-xl">
+          <button className=" h-6 w-6 ">
+            <Image src="./search.svg" alt="" height={24} width={24} />
+          </button>
+          <button className="hidden md:flex  h-6 w-6">
+            <Image src="./heart.svg" alt="" height={24} width={24} />
+          </button>
+          <button className=" h-6 w-6">
+            <Image src="./cart.svg" alt="" height={24} width={24} />
+          </button>
+          <button className=" h-6 w-6">
+            <Image src="./user.svg" alt="" height={24} width={24} />
+          </button>
+
+          <div className="icon_small flex  md:hidden">
+            <div className="flex ">
+              <Menu
+                show={show}
+                setShow={setShow}
+                isRotated={isRotated}
+                setIsRotated={setIsRotated}
+              />
+            </div>
+          </div>
+        </div>
+
+        {/* md-devices */}
+        {/* <div className="iconify-slide"></div> */}
+      </div>
+    </motion.nav>
+  );
+}
